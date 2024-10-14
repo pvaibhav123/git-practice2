4a412b26ab8bb2da9e9b6d13ef746cc8ab20470c
Vaibhav <vaibhavpanwalkar39@gmail.com>
2024-10-14 20:38:28 +0530

    add all files into Git

---
+++ src/test/java/com/jenkinsPractice/JenkinsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
